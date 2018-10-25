9c251f6b462b6579dfc14513eb018dad2f2bcb4f
Oleksii Khilkevych <oleksii.khilkevych@osi.ca.gov>
2018-10-24 17:00:30 -0700

    feat(test): 6th test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React changes 5
+            Learn React changes 6
           </a>
         </header>
       </div>
