ea9ca3fbbaa53f6e7235d07f0d71aa6f89e55975
Oleksii Khilkevych <oleksii.khilkevych@osi.ca.gov>
2018-10-26 14:25:25 -0700

    feat(ggg): 28

--- libs/khilkevych-core/src/App.js
+++ libs/khilkevych-core/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 2
+            Learn React 27
           </a>
         </header>
       </div>
