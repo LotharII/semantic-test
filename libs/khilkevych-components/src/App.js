efb18b3e8219d6a2a341498deeb17aacbb3ee7f2
Oleksii Khilkevych <oleksii.khilkevych@osi.ca.gov>
2018-10-31 14:58:58 -0700

    feat(foo): blah

--- libs/khilkevych-components/src/App.js
+++ libs/khilkevych-components/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 38mfnndcdcsxsаfwerdfdwdwh
+            Learn React 38mfnndcdcsxsаfwerdfdwdwhdf
           </a>
         </header>
       </div>
